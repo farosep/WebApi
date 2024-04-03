4f5a30947224af1d2dade8e9d3f700daf76124e8
farosep <you@example.com>
2024-04-03 22:42:51 +0700

    models full

--- api/Models/Comment.cs
+++ api/Models/Comment.cs
@@ -7,6 +7,15 @@ namespace api.Models
 {
     public class Comment
     {
-        
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public string Content { get; set; } = string.Empty;
+
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int? StockId { get; set; }
+        // navigation 
+        public Stock? Stock { get; set; }
+
     }
 }
