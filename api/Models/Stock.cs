478d6cbb112f4141e266453e0de92774686bca1f
farosep <you@example.com>
2024-04-03 22:37:45 +0700

    models.half

---
+++ api/Models/Stock.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace api.Models
+{
+    public class Stock
+    {
+        public Guid Id { get; set; }
+
+        public string Symbol { get; set; } = string.Empty;
+
+        public string CompanyName { get; set; } = string.Empty;
+
+        [Column(TypeName = "dicimal(18,2)")]
+        public decimal Purchase { get; set; }
+
+        [Column(TypeName = "dicimal(18,2)")]
+        public decimal LastDiv { get; set; }
+
+        public string Industry { get; set; } = string.Empty;
+
+        public long MarketCap { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
+    }
+}
